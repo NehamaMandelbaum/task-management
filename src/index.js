bc27db80acf5c2a1c999f66904e5f5fa03fba343
NehamaMandelbaum <nehama1836@gmail.com>
2021-06-09 11:44:38 -0300

    desafio finalizado

--- src/index.js
+++ src/index.js
@@ -101,15 +101,15 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const {user} = request
   const {id} = request.params
 
-  const todo = user.todos.findIndex((todo) => todo.id=== id)
+  const todoIndex = user.todos.findIndex((todo) => todo.id=== id)
 
-  if(todo === -1){
+  if(todoIndex === -1){
     response.status(404).json({error: "To do does not exist!"})
   }
 
-  users.todos.splice(todo, 1)
+  user.todos.splice(todoIndex, 1)
 
-  return response.status(204).send()
+  return response.status(204).json()
 
 });
 
